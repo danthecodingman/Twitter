912d932754f786063ee9e9c53a29d6dc71fd7ab8
Daniel Avila <danielavila@fb.com>
2019-07-03 15:51:31 -0700

    none

---
+++ twitter/ComposeViewControllerCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  ComposeViewControllerCollectionViewController.h
+//  twitter
+//
+//  Created by danielavila on 7/3/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewControllerCollectionViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
