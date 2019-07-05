2f060b2d28885aa87d03632c2648718841d56c0c
Daniel Avila <danielavila@fb.com>
2019-07-05 14:08:48 -0700

    name

---
+++ twitter/API/TweetCell.m
@@ -0,0 +1,23 @@
+//
+//  TweetCell.m
+//  
+//
+//  Created by danielavila on 7/2/19.
+//
+
+#import "TweetCell.h"
+
+@implementation TweetCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+//dka 2 custom cell
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -7,14 +7,26 @@
 //
 
 #import "ComposeViewController.h"
-
+#import "APIManager.h"
 @interface ComposeViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *myBox;
 
 @end
 
 @implementation ComposeViewController
 
 - (IBAction)cancel:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+- (IBAction)post:(id)sender {
+    [[APIManager shared] postStatusWithText:_myBox.text completion:^(Tweet *tweets, NSError *error) {// step 4: make an API request
+        if (tweets) {
+            NSLog(@"Works getting home");
+        } else {
+            NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
+        }
+        
+    }];
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
